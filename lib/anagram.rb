d88ac9198866daa54c89081ce2d5d180afcf2df8
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-13 19:26:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,8 @@ class Anagram
   
   
   def match(word)
-    word.each {|mix| mix.split("").sort}
-    binding.pry
+    word.each {|mix| mix.split("").sort == word.split("").sort}
+
   end
   
 end
