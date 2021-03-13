ef387748253bbd7041b002bb09956b22851992c1
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-13 18:24:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+
+
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  answer = []
+  
+  def match(words)
+    words = %w()
+    words.each do |mix|
+      mix.split("").sort 
+      if mix.sort == word.sort 
+      answer << mix
+    end  
+    end
+  end 
+  answer
+  
+end
